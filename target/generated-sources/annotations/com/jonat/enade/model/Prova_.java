888eb2dc83b49cb406527d4dfc5fe7d4c1e43e7a
Your-Full-Name <jonatascarvalhodomingos@hotmail.com>
2021-06-27 22:41:48 -0300

    Troca de fonte

--- target/generated-sources/annotations/com/jonat/enade/model/Prova_.java
+++ target/generated-sources/annotations/com/jonat/enade/model/Prova_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-06-27T22:04:22")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-06-27T22:38:35")
 @StaticMetamodel(Prova.class)
 public class Prova_ { 
 
